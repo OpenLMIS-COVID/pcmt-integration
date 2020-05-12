e8792d9cc50a838b2a2056c34143375977ad516a
wojtekbu <wbulawa@soldevelo.com>
2020-05-12 11:16:19 +0200

    COV-53: Clean imports at OrderableIntegrationSendTaskTest

--- src/test/java/org/openlmis/integration/pcmt/service/send/OrderableIntegrationSendTaskTest.java
+++ src/test/java/org/openlmis/integration/pcmt/service/send/OrderableIntegrationSendTaskTest.java
@@ -19,7 +19,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Clock;
-import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.UUID;
